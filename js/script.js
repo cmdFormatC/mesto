990d3fcd14676798deeb32dfbf1244cda6f48107
Bragin Fedor <bragin.fedor@gmail.com>
2023-03-29 13:57:04 +0400

    2.0

--- js/script.js
+++ js/script.js
@@ -2,23 +2,27 @@ let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__close-button');
 let openButton = document.querySelector('.profile__edit-button');
 let formElement = popup.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__description');
-let name = document.querySelector('.profile__name');
+let nameInput = formElement.querySelector('.popup__input_name');
+let jobInput = formElement.querySelector('.popup__input_description');
+let profileName = document.querySelector('.profile__name');
 let job = document.querySelector('.profile__description');
-let likeButton = ''
+let likeButton = '';
+console.log(profileName.textContent);
+console.log(job.textContent);
 const elements = document.querySelector('.elements');
 function popupClose() {
-    popup.classList.add('closed');
+    popup.classList.add('popup_closed');
 }
 
 function popupOpen() {
-    popup.classList.remove('closed');
+    popup.classList.remove('popup_closed');
+    nameInput.value = profileName.textContent
+    jobInput.value = job.textContent
 }
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    name.textContent = nameInput.value;
+    profileName.textContent = nameInput.value;
     job.textContent = jobInput.value;
     popupClose()
 }
