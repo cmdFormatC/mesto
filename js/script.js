0f6af443f88c1b5f3431ae2150ddcb0efa0dd1fd
Bragin Fedor <bragin.fedor@gmail.com>
2023-04-10 14:55:13 +0400

    fixed1.0

--- js/script.js
+++ js/script.js
@@ -5,18 +5,18 @@ const addButton = document.querySelector('.profile__add-button');
 const elements = document.querySelector('.elements');
 const popupEditProfile = document.querySelector('.popup_edit-profile');
 const popupAddCard = document.querySelector('.popup_add-card');
-const popupImage = document.querySelector('.popup-mesto');
+const popupImage = document.querySelector('.popup_zoom-image');
 const popupEditProfileForm = popupEditProfile.querySelector('.popup__form');
 const popupAddCardForm = popupAddCard.querySelector('.popup__form');
-let popupInputProfileName = popupEditProfile.querySelector('.popup__input_profile_name');
-let popupInputProfileDescription = popupEditProfile.querySelector('.popup__input_profile_description');
+const popupInputProfileName = popupEditProfile.querySelector('.popup__input_profile_name');
+const popupInputProfileDescription = popupEditProfile.querySelector('.popup__input_profile_description');
 const popupProfileCloseButton = popupEditProfile.querySelector('.popup__close-button');
 const popupAddCloseButton = popupAddCard.querySelector('.popup__close-button');
 const popupImageCloseButton = popupImage.querySelector('.popup__close-button');
-const popupScaledImage = popupImage.querySelector('.popup-mesto__image');
-const popupImageCaption = popupImage.querySelector('.popup-mesto__caption');
-let popupInputCardUrl = popupAddCard.querySelector('.popup__input_card_url');
-let popupInputCardName = popupAddCard.querySelector('.popup__input_card_name');
+const popupScaledImage = popupImage.querySelector('.popup_zoom-image__image');
+const popupImageCaption = popupImage.querySelector('.popup_zoom-image__caption');
+const popupInputCardUrl = popupAddCard.querySelector('.popup__input_card_url');
+const popupInputCardName = popupAddCard.querySelector('.popup__input_card_name');
 
 const initialCards = [
     {
@@ -60,59 +60,59 @@ function profileFormSubmit (evt) {
     closePopup(popupEditProfile);
 }
 function openPopup (popup) {
-    const openButton = popup.target;
-    if (openButton.classList.contains('profile__edit-button') === true) {
-        popupEditProfile.classList.remove('popup_closed');
-        popupInputProfileName.value = profileName.textContent;
-        popupInputProfileDescription.value = profileJob.textContent;
-    } else if (openButton.classList.contains('profile__add-button') === true) {
-        popupAddCard.classList.remove('popup_closed');
-    } else if (openButton.classList.contains('element__image') === true) {
-        popupImage.classList.remove('popup_closed');
-        popupScaledImage.src = openButton.src;
-        popupImageCaption.textContent = openButton.parentNode.querySelector('.element__title').textContent;
-    } 
+    popup.classList.add('popup_opened');
 }
 function closePopup (popup) {
-    if (popup.classList.contains('popup_edit-profile') === true) {
-        popupEditProfile.classList.add('popup_closed');
-    } else if (popup.classList.contains('popup_add-card') === true) {
-        popupAddCard.classList.add('popup_closed');
-    } else if (popup.classList.contains('popup_image') === true) {
-        popupImage.classList.add('popup_closed');
-    }
+    popup.classList.remove('popup_opened');
+    popup.setAttribute("style",  "transition: visibility 0.3s, opacity 0.3s linear;");
 }
 
-function createCard (evt) {
-    evt.preventDefault();
-    let card = document.querySelector('.element').cloneNode(true);
-    const firstElement = elements.firstChild.nextSibling;
+function createCard () {
+    const card = document.querySelector('.element').cloneNode(true);
     card.querySelector('.element__image').src = popupInputCardUrl.value;
     card.querySelector('.element__title').textContent = popupInputCardName.value;
-    elements.insertBefore(card, firstElement);
-    closePopup(popupAddCard);
-    popupInputCardUrl.value = '';
-    popupInputCardName.value = '';
+    return card
+}
+function addFormSubmit (evt) {
+  evt.preventDefault(); 
+  const newCard = createCard ();
+  const firstElement = elements.firstChild.nextSibling;
+  console.log(newCard)
+  elements.insertBefore(newCard, firstElement); 
+  closePopup(popupAddCard);
+  popupInputCardUrl.value = '';
+  popupInputCardName.value = '';
+}
+function openPopupProfileEdit() {
+  popupInputProfileName.value = profileName.textContent;
+  popupInputProfileDescription.value = profileJob.textContent;
+  openPopup(popupEditProfile);
+}
+function zoomCard(e) {
+  if (e.target.classList.contains('element__image') === true) {
+    popupScaledImage.src = e.target.src;
+    popupImageCaption.textContent = e.target.parentNode.querySelector('.element__title').textContent;
+    openPopup(popupImage);
+  }
 }
 
-
 function handleClick(e) {
-    let elementButton = e.target;
+    const elementButton = e.target;
     if (elementButton.classList.contains('element__like-button_active') === true) {
         elementButton.classList.remove('element__like-button_active');
     } else if (elementButton.classList.contains('element__like-button') === true) {
         elementButton.classList.add('element__like-button_active');
     } else if (elementButton.classList.contains('element__delete-button') === true) {
-        let deletedElement = e.target.parentElement;
+        const deletedElement = e.target.parentElement;
         deletedElement.remove();
     } 
   }
 elements.addEventListener('click', handleClick);
-addButton.addEventListener('click', openPopup);
-editButton.addEventListener('click', openPopup);
+addButton.addEventListener('click', () => openPopup (popupAddCard));
+editButton.addEventListener('click', openPopupProfileEdit);
 popupEditProfileForm.addEventListener('submit', profileFormSubmit);
 popupProfileCloseButton.addEventListener('click', () => closePopup (popupEditProfile));
 popupAddCloseButton.addEventListener('click', () => closePopup (popupAddCard));
 popupImageCloseButton.addEventListener('click', () => closePopup (popupImage));
-elements.addEventListener('click', openPopup);
-popupAddCardForm.addEventListener('submit', createCard);
+elements.addEventListener('click', zoomCard);
+popupAddCardForm.addEventListener('submit', addFormSubmit);
